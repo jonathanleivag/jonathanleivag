8de38d07747f7c536094c078f8c52677319b264d
jonathanleivag <email@jonathanleivag.cl>
2022-05-03 15:57:53 -0400

    configuración de vscode y eslint

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
+
+const ESLintPlugin = require('eslint-webpack-plugin')
+
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    if (dev) {
+      config.plugins.push(
+        new ESLintPlugin({
+          files: ['**/*.{ts,tsx,js,jsx}'],
+          exclude: ['node_modules'],
+          extensions: ['ts', 'tsx', 'js', 'jsx']
+        })
+      )
+    }
+    return config
+  }
 }
 
 module.exports = nextConfig
