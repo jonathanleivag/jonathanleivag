2931b294e7913ca0087b075f7fbbe45971005c61
jonathanleivag <email@jonathanleivag.cl>
2022-05-17 23:30:38 -0400

    redes sociales

--- components/ui/UiSocialComponent.tsx
+++ components/ui/UiSocialComponent.tsx
@@ -26,15 +26,19 @@ export const UiSocialComponent: FC<IUiSocialComponentProps> = ({
       <a href={NEXT_PUBLIC_LINKEDIN} target='_blank' rel='noreferrer'>
         <AiFillLinkedin className={`hover:text-linkedin ${className}`} />
       </a>
+
       <a href={NEXT_PUBLIC_FACEBOOK} target='_blank' rel='noreferrer'>
         <AiFillFacebook className={`hover:text-facebook ${className}`} />
       </a>
+
       <a href={NEXT_PUBLIC_INSTAGRAM} target='_blank' rel='noreferrer'>
         <AiFillInstagram className={`hover:text-instagram ${className}`} />
       </a>
+
       <a href={NEXT_PUBLIC_TWITTER} target='_blank' rel='noreferrer'>
         <AiFillTwitterSquare className={`hover:text-twitter ${className}`} />
       </a>
+
       <a href={NEXT_PUBLIC_YOUTUBE} target='_blank' rel='noreferrer'>
         <AiFillYoutube className={`hover:text-youtube ${className}`} />
       </a>
