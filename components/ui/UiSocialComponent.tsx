a1e98ec84275ad4ec905088cdd79204bd280ebe8
jonathanleivag <email@jonathanleivag.cl>
2022-05-17 23:27:18 -0400

    redes sociales

--- components/ui/UiSocialComponent.tsx
+++ components/ui/UiSocialComponent.tsx
@@ -6,6 +6,13 @@ import {
   AiFillTwitterSquare,
   AiFillYoutube
 } from 'react-icons/ai'
+import {
+  NEXT_PUBLIC_FACEBOOK,
+  NEXT_PUBLIC_INSTAGRAM,
+  NEXT_PUBLIC_LINKEDIN,
+  NEXT_PUBLIC_TWITTER,
+  NEXT_PUBLIC_YOUTUBE
+} from '../../utils'
 
 export interface IUiSocialComponentProps {
   className?: string
@@ -16,11 +23,21 @@ export const UiSocialComponent: FC<IUiSocialComponentProps> = ({
 }) => {
   return (
     <div className='mx-3 flex flex-row items-center gap-2'>
-      <AiFillLinkedin className={`hover:text-linkedin ${className}`} />
-      <AiFillFacebook className={`hover:text-facebook ${className}`} />
-      <AiFillInstagram className={`hover:text-instagram ${className}`} />
-      <AiFillTwitterSquare className={`hover:text-twitter ${className}`} />
-      <AiFillYoutube className={`hover:text-youtube ${className}`} />
+      <a href={NEXT_PUBLIC_LINKEDIN} target='_blank' rel='noreferrer'>
+        <AiFillLinkedin className={`hover:text-linkedin ${className}`} />
+      </a>
+      <a href={NEXT_PUBLIC_FACEBOOK} target='_blank' rel='noreferrer'>
+        <AiFillFacebook className={`hover:text-facebook ${className}`} />
+      </a>
+      <a href={NEXT_PUBLIC_INSTAGRAM} target='_blank' rel='noreferrer'>
+        <AiFillInstagram className={`hover:text-instagram ${className}`} />
+      </a>
+      <a href={NEXT_PUBLIC_TWITTER} target='_blank' rel='noreferrer'>
+        <AiFillTwitterSquare className={`hover:text-twitter ${className}`} />
+      </a>
+      <a href={NEXT_PUBLIC_YOUTUBE} target='_blank' rel='noreferrer'>
+        <AiFillYoutube className={`hover:text-youtube ${className}`} />
+      </a>
     </div>
   )
 }
--- utils/envUtil.ts
+++ utils/envUtil.ts
@@ -1,1 +1,11 @@
 export const NEXT_PUBLIC_URL_API: string = process.env.NEXT_PUBLIC_URL_API || ''
+
+// redes sociales
+export const NEXT_PUBLIC_INSTAGRAM: string =
+  process.env.NEXT_PUBLIC_INSTAGRAM || ''
+export const NEXT_PUBLIC_FACEBOOK: string =
+  process.env.NEXT_PUBLIC_FACEBOOK || ''
+export const NEXT_PUBLIC_TWITTER: string = process.env.NEXT_PUBLIC_TWITTER || ''
+export const NEXT_PUBLIC_YOUTUBE: string = process.env.NEXT_PUBLIC_YOUTUBE || ''
+export const NEXT_PUBLIC_LINKEDIN: string =
+  process.env.NEXT_PUBLIC_LINKEDIN || ''
