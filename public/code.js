0219976cdb2701696716a654aaa86ff40eb800ef
jonathanleivag <email@jonathanleivag.cl>
2022-06-19 22:40:35 -0400

    code

---
+++ public/code.js
@@ -0,0 +1,23 @@
+console.log('Hello World')
+
+const jonathanleivag = {
+  pais: 'Chile 🇨🇱',
+  edad: 26,
+  idioma: ['Español', 'un pelin de Ingles'],
+  programacion: {
+    so: ['Mac', 'IOS', 'ANDROID', 'WINDOWS'],
+    herramientas: 'VS Code',
+    frontEnd: {
+      lenguaje: ['JavaScript', 'TypeScript'],
+      framework: ['Next js', 'React Native'],
+      libreria: ['React', 'Tailwind css', 'NativeBase']
+    },
+    backEnd: {
+      lenguaje: ['JavaScript', 'TypeScript'],
+      framework: ['Express']
+    },
+    baseDeDatos: ['Mongo db', 'MySql']
+  }
+}
+
+console.log(jonathanleivag)
