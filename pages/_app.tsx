6a70f072a40f240e722880eaa1eba81bb0c7163e
jonathanleivag <email@jonathanleivag.cl>
2022-06-21 02:26:55 -0400

    No seleccionar texto ni hacer click derecho

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
 import { AppProps } from 'next/app'
+import { useEffect } from 'react'
 import { Provider } from 'react-redux'
 import { store } from '../app/store'
 import '../styles/fonts/montserrat/montserrat.css'
 import '../styles/globals.css'
 
 function MyApp ({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    document.oncontextmenu = function () {
+      return false
+    }
+    return () => {}
+  }, [])
+
   return (
     <Provider store={store}>
       <Component {...pageProps} />
